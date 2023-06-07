429d6f7203791268ed9876efb2d03b1739547928
bytarik <bydemirel.root@hotmail.com>
2023-06-07 19:24:47 +0300

    commit1

---
+++ DictOnPython/Dict.py
@@ -0,0 +1,14 @@
+
+'''I have a Dictionary like below'''
+
+DictOne = {
+    'İsim':'Tarık',
+    'Yaş':'22',
+    'Cinsiyet':'Erkek'
+       }
+DictTwo = {}
+
+
+'''And now we want keys name -How we get keys name'''
+#Method 1 - with 'keys()' method of dictionary
+DictOne.keys()
